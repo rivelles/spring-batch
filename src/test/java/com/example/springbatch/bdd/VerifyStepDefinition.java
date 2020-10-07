4eecd5337e51b209a641785493291b50a05988e2
Lucas <rivelleslucas@gmail.com>
2020-10-07 15:46:16 -0300

    Updating test

--- src/test/java/com/example/springbatch/bdd/VerifyStepDefinition.java
+++ src/test/java/com/example/springbatch/bdd/VerifyStepDefinition.java
@@ -5,10 +5,20 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.AssertFile;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -24,9 +34,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class VerifyStepDefinition {
 
-    @Given("a set of data is read in")
-    public void loadData() {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
+    private JobExecution jobExecution;
+
+    @Given("a set of data is read in")
+    public void loadData() throws Exception {
+        jobExecution = jobLauncherTestUtils.launchJob();
     }
 
     @When("that data is processed")
@@ -36,6 +51,7 @@ public class VerifyStepDefinition {
 
     @Then("output is writen")
     public void writeOutput() {
+        Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
 
     }
 }
